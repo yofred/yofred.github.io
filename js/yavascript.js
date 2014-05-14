f5395ef4ae08fc1aa7b6fa64d069d7eb5ef59ce8
Yofred Moik <ytmoik@gmail.com>
2014-05-14 01:00:35 -0400

    jquery slide effect

--- js/yavascript.js
+++ js/yavascript.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
 
-  $("#resume").hide();
   $(".fancybox").fancybox();
+  
+  $("#resume").hide();
+  $("#work-button").on('click', function(){
+    $("#resume").hide();
+    $('#work').slideDown('slow')
+  });
+  
+  $("#resume-button").on('click', function(){
+    $("#work").hide();
+    $('#resume').slideDown('slow')
+  });
 
   
   $('.hover').hover(function(){
