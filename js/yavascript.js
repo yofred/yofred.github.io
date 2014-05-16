d52c8141c7bc00fe0da8d301a72c173971506e40
Yofred Moik <ytmoik@gmail.com>
2014-05-16 00:25:53 -0400

    added hover effect for portfolio images and links

--- js/yavascript.js
+++ js/yavascript.js
@@ -14,6 +14,53 @@ $(document).ready(function(){
     $('#resume').slideDown('slow')
   });
   
+  // hover effect for portfolio images
+  $("#main-image").mouseover(function(){
+    $("#main-image .view").css("display", "block");
+  });
+  $("#main-image").mouseleave(function(){
+    $("#main-image .view").css("display", "none");
+  });
+  $("#image-1").mouseover(function(){
+    $("#image-1 .view").css("display", "block");
+  });
+  $("#image-1").mouseleave(function(){
+    $("#image-1 .view").css("display", "none");
+  });
+  $("#image-2").mouseover(function(){
+    $("#image-2 .view").css("display", "block");
+  });
+  $("#image-2").mouseleave(function(){
+    $("#image-2 .view").css("display", "none");
+  });
+  $("#image-3").mouseover(function(){
+    $("#image-3 .view").css("display", "block");
+  });
+  $("#image-3").mouseleave(function(){
+    $("#image-3 .view").css("display", "none");
+  });
+  $("#image-4").mouseover(function(){
+    $("#image-4 .view").css("display", "block");
+  });
+  $("#image-4").mouseleave(function(){
+    $("#image-4 .view").css("display", "none");
+  });
+  $("#image-5").mouseover(function(){
+    $("#image-5 .view").css("display", "block");
+  });
+  $("#image-5").mouseleave(function(){
+    $("#image-5 .view").css("display", "none");
+  });
+  $("#image-6").mouseover(function(){
+    $("#image-6 .view").css("display", "block");
+  });
+  $("#image-6").mouseleave(function(){
+    $("#image-6 .view").css("display", "none");
+  });
+  
+  
+  
+  
   // $("#resume-project-button").on('click', function(){
   //    $("#work").hide();
   //    $('#resume').slideDown('slow')
@@ -36,32 +83,3 @@ $(document).ready(function(){
   });
   
 });
-
-
-// $(document).ready(function(){
-// 
-//  $(".fancybox").fancybox();
-//   
-//   $("#work-button").on('click', function(){
-//     $("#resume").hide();
-//     $('#work').slideDown('slow')
-//   });
-//   
-//   $("#resume-button").on('click', function(){
-//     $("#work").hide();
-//     $('#resume').slideDown('slow');
-//   });
-//   
-//   $("#resume-project-button").on('click', function(){
-//      $("#work").hide();
-//      $('#resume').show();
-//   });
-// 
-//   
-//   $('.hover').hover(function(){
-//       $(this).addClass('flip');
-//   },function(){
-//       $(this).removeClass('flip');
-//   });
-//   
-// });
